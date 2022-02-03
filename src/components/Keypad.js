61e58c22c183323bffd64b301cd3336049e04f11
Alison Melendez <alisonmelendez132@gmail.com>
2022-02-03 16:11:36 -0500

    commit tree b8110f3289e6a7ba39df555c064ecfdcc143f8d5

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,9 @@
 // Code Keypad Component Here
+
+function Keypad(){
+    return (
+        <input onChange={()=> console.log('Entering password...')}type="password"></input>
+    );
+}
+
+export default Keypad; 
